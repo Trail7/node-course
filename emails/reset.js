9ebea2647e14dddb81090339a72239078de2c854
Arthur Harmash <usd@Arturs-MacBook-Pro.local>
2022-01-25 23:23:10 +0200

    lesson 67 - reset password email

---
+++ emails/reset.js
@@ -0,0 +1,17 @@
+const keys = require ('../keys')
+
+module.exports = function(email, token) {
+    return {
+        to: email,
+        from: keys.EMAIL_FROM,
+        subject: 'Reset Password',
+        html: `
+        <h1>Forgot password?</h1>
+        <p>If it was not you please disregard this email</p>
+        <p>To restore you access to the site click on the following link</p>
+        <p><a href="${keys.BASE_URL}/auth/password${token}">Reset password</a></p>
+        <hr />
+        <a href="${keys.BASE_URL}">Test Store</a>
+        `
+    }
+}
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ const nodemailer = require ('nodemailer')
 const sendgrid  = require('nodemailer-sendgrid-transport')
 const keys = require('../keys')
 const regEmail = require('../emails/registration')
+const resetEmail = require('../emails/reset')
+const crypto = require('crypto')
 
 const transporter = nodemailer.createTransport(sendgrid({
     auth: {api_key: keys.SENDGRID_API_KEY}
@@ -79,6 +81,72 @@ router.post('/register', async (req, res) => {
     } catch (e) {
         console.log(e)
     }
+})
+
+router.get('/reset',  (req, res) =>{
+    res.render('auth/reset', {
+        title: 'reset pw',
+        error: req.flash('error')
+    })
+})
+
+router.post('/reset',  (req, res) => {
+
+    try {
+        crypto.randomBytes(32, async (err, buffer) => {
+            if (err) {
+                req.flash('error', 'smth went terribly wrong')
+                return res.redirect('/auth/reset')
+            }
+
+            const token = buffer.toString("hex")
+
+            const candidate = await User.findOne({email: req.body.email})
+
+            if (candidate) {
+                candidate.resetToken = token
+                candidate.resetTokenExp = Date.now() + 3600*1000
+                await candidate.save()
+                await transporter.sendMail(resetEmail(candidate.email, token))
+                res.redirect('/auth/login')
+            } else {
+                req.flash('error', 'user with such email does not exist')
+                res.redirect('/auth/reset')
+            }
+        })
+    }catch (e) {
+        console.log(e)
+    }
+})
+
+router.get('/password/:token',  async (req, res) =>{
+    if (!req.params.token) {
+        res.redirect('/auth/login')
+    }
+
+    try{
+        const user = await User.findOne({
+            resetToken: req.params.token,
+            resetTokenExp: {$gt: Date.now}
+        })
+
+        if (!user) {
+            res.redirect('/auth/login')
+        } else {
+            res.render('auth/password', {
+                title: 'restore access',
+                error: req.flash('error'),
+                userId: user._id.toString(),
+                token: req.params.token
+            })
+        }
+
+
+
+    }catch(e) {
+        console.log(e)
+    }
+
 })
 
 
