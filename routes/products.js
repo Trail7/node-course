8cd6cd701e432c3cb85ea3ea557c3a46aa610e23
Arthur Harmash <usd@Arturs-MacBook-Pro.local>
2022-01-30 22:21:42 +0200

    lesson 70 - refactoring

--- routes/products.js
+++ routes/products.js
@@ -59,18 +59,27 @@ router.post('/edit', auth, async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    res.render('product', {
-        layout: 'empty',
-        title: `product ${product.title}`,
-        product
-    })
+    try{
+        const product = await Product.findById(req.params.id)
+        res.render('product', {
+            layout: 'empty',
+            title: `product ${product.title}`,
+            product
+        })
+    } catch(e) {
+        console.log(e)
+    }
+
+
 })
 
 router.post('/remove', auth, async (req, res) => {
 
     try {
-        await Product.deleteOne({_id: req.body.id})
+        await Product.deleteOne({
+            _id: req.body.id,
+            userId: req.user._id
+        })
         res.redirect('/products')
     } catch (e) {
         console.log(e)
